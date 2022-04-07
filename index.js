b2bd8c6abcf9607c7926aa3b870db3fe6c0cdf36
Tanja Rannikko <tanja.rannikko@tuni.fi>
2022-04-07 11:31:24 +0300

    Add database connection.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-
+const mysql = require("mysql");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 const db = [{ name: "tiina" }, { name: "jack" }];
+
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+};
+
+var connection = mysql.createConnection(config);
+app.get("/", (req, res) => {
+  connection.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
 app.get("/names", (req, res) => {
   res.send(db);
@@ -10,4 +28,4 @@ app.get("/names", (req, res) => {
 
 const server = app.listen(port, () => {
   console.log(`Listening on port ${server.address().port}`);
-}); 
+});

e67c340b841d4a00c5c30a6a61b1f13eb7ab86e1
Tanja Rannikko <tanja.rannikko@tuni.fi>
2022-04-09 11:38:27 +0300

    04: (07): Add map with Markers.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,28 +2,48 @@
  * @author Jussi Pohjolainen
  */
 
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 class App extends React.Component {
   state = { locations: [] };
+
   async componentDidMount() {
     let hr = await fetch("http://localhost:8080/locations");
     let json = await hr.json();
     this.setState({ locations: json });
   }
+
   render() {
     if (this.state.locations.length === 0) {
-      return <p>loading...</p>;
+      return (
+        <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      );
     } else {
-      let ui = this.state.locations.map((loc) => (
-        <li key={loc.id}>
-          {loc.id} - {loc.latitude} - {loc.longitude}
-        </li>
+      let markers = this.state.locations.map((loc) => (
+        <Marker key={loc.id} position={[loc.latitude, loc.longitude]}>
+          <Popup>
+            Location {loc.id}. <br /> lat: {loc.latitude}, lon: {loc.longitude}.
+          </Popup>
+        </Marker>
       ));
-      return <ul>{ui}</ul>;
+
+      return (
+        <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <>{markers}</>
+        </MapContainer>
+      );
     }
   }
 }
+
 export default App;
